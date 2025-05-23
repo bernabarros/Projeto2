587bdf54f0b9641725cb396762b11738d727620b
bernabarros <bernardobarros.work@gmail.com>
2025-05-23 16:51:14 +0100

    Add Recipe class code

--- MealPlanner/Model/Ingredient.cs
+++ MealPlanner/Model/Ingredient.cs
@@ -5,8 +5,33 @@ using System.Threading.Tasks;
 
 namespace MealPlanner.Model
 {
-    public class Ingredient
+    public class Ingredient : IIngredient
     {
-        
+        public string Name { get; }
+        public string Type { get; }
+
+        public Ingredient(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public bool Equals(IIngredient other)
+        {
+            bool equal_value;
+            Ingredient otherIngredient = other as Ingredient;
+            if (otherIngredient is null)
+                equal_value = false;
+            else if (Type == otherIngredient.Type &&
+            Name == otherIngredient.Name)
+            {
+                equal_value = true;
+            }
+            else
+            {
+                equal_value = false;
+            }
+            return equal_value;
+        }
     }
 }
--- MealPlanner/Model/Recipe.cs
+++ MealPlanner/Model/Recipe.cs
@@ -5,8 +5,33 @@ using System.Threading.Tasks;
 
 namespace MealPlanner.Model
 {
-    public class Recipe
+    public class Recipe : IRecipe
     {
-        
+        public string Name { get; }
+
+        public IReadOnlyDictionary<IIngredient, int> IngredientsNeeded { get; }
+
+        public double SuccessRate{ get; }
+
+        public Recipe(string name, double successRate)
+        {
+            Name = name;
+            SuccessRate = successRate;
+        }
+
+        public int CompareTo(IRecipe other)
+        {
+            int compare_value = 1;
+
+            if (other == null)
+            {
+                compare_value = 1;
+            }
+            else if (other != null)
+            {
+                compare_value = this.Name.CompareTo(other.Name);
+            }
+            return compare_value;
+        }
     }
 }
