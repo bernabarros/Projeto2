a0730d319ed06e2e80b62b21e831f6493d1d3882
bernabarros <bernardobarros.work@gmail.com>
2025-05-23 16:29:11 +0100

    Create Ingredient class and add code, Create Recipe class

---
+++ MealPlanner/Model/Ingredient.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MealPlanner.Model
+{
+    public class Ingredient
+    {
+        
+    }
+}
